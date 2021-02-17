70a3846b9cc2e211b4cda3a793ed341070ffbe2e
Achmad Fathoni <fathoni.id@gmail.com>
2021-02-17 23:16:15 +0700

    Success running

--- slam_launch.sh
+++ slam_launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source ../devel/setup.bash
+source /home/toni/Documents/source_linux/ros_ws/oslam/devel/setup.bash
 
-BAGPATH='.../OS1-16_city1.bag' #own path
-CONFIGPATH='.../sample_config_file_16.json' #own path
+BAGPATH='/home/toni/Downloads/OS2_32G_ROSbag/OS2_32G_1024x20_sample.bag' #own path
+CONFIGPATH='/home/toni/Downloads/OS2_32G_ROSbag/OS2_32G_1024x20.json' #own path
 
-CARTOCONF='.../src/ouster_slam/' #own path
+CARTOCONF='/home/toni/Documents/source_linux/ros_ws/oslam/src/ouster_slam' #own path
 
 roslaunch ouster_slam os_slam.launch \
                              udp_hostname:=127.0.0.1 \
